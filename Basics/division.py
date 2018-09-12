781d4f4c190d34ba81f0f9740d5ea77fdff7adae
virajs <viraj.shelke@merce.co>
2018-09-12 16:31:01 +0530

    division

---
+++ Basics/division.py
@@ -0,0 +1,24 @@
+# without user input
+# using three variables
+a = 20
+b = 10
+c = a / b
+print "The division of the numbers is", c
+
+# using 2 variables
+a = 20 
+b = 10
+print "The division of the numbers is", a / b
+
+# with user input 
+# using three variables
+a = int(input("Enter first number "))
+b = int(input("Enter first number "))
+c = a / b
+print "The division of the numbers is", c
+
+# using 2 variables
+# int() - will convert the input into numbers if string is passed!
+a = int(input("Enter first number "))
+b = int(input("Enter first number "))
+print "The division of the numbers is", a / b
