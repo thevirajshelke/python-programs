3414f5cadac3c83b96a920ad8fc7cd097c6ec24c
virajs <viraj.shelke@merce.co>
2018-09-12 16:31:08 +0530

    multiplication

---
+++ Basics/multiplication.py
@@ -0,0 +1,24 @@
+# without user input
+# using three variables
+a = 10
+b = 20
+c = a * b
+print "The multiplication of the numbers is", c
+
+# using 2 variables
+a = 10 
+b = 20
+print "The multiplication of the numbers is", a * b
+
+# with user input 
+# using three variables
+a = int(input("Enter first number "))
+b = int(input("Enter first number "))
+c = a * b
+print "The multiplication of the numbers is", c
+
+# using 2 variables
+# int() - will convert the input into numbers if string is passed!
+a = int(input("Enter first number "))
+b = int(input("Enter first number "))
+print "The multiplication of the numbers is", a * b
